694a953fe0a13319299a8a764b1b2eae888995f3
Ricky Liu <101468308+ScripterRickster@users.noreply.github.com>
2025-07-05 19:32:33 -0400

    Update AdminSettings.lua

--- AdminSettings.lua
+++ AdminSettings.lua
@@ -3,13 +3,46 @@ local module = {}
 module.Prefix = "!"
 
 module.Ranks = { -- ranks for the admin system to use
-	--{rank id, rank name}
-	{5,"Owner"};
-	{4,"Head Administrator"};
-	{3,"Developer"};
-	{2,"Administrator"};
-	{1,"Moderator"};
-	{0,"Regular"};
+	
+	--[[
+	
+	[rank_name] = { -- rank name
+		RankId = 0; -- rank id
+		RankColour = Color3.fromRGB(255,255,255); -- rank colour (optional parameter) | not including this means the user will not have a chat tag and an overhead tag if enabled
+	}
+	
+	]]
+	
+	["Owner"] = { 
+		RankId = 5;
+		RankColour = Color3.fromRGB(0, 120, 180);
+		
+	};
+	["Head Administrator"] = { 
+		RankId = 4;
+		RankColour = Color3.fromRGB(85, 255, 0);
+
+	};
+	["Developer"] = { 
+		RankId = 3;
+		RankColour = Color3.fromRGB(255, 170, 0);
+
+	};
+	["Administrator"] = { 
+		RankId = 2;
+		RankColour = Color3.fromRGB(255, 0, 0);
+
+	};
+	["Moderator"] = { 
+		RankId = 1;
+		RankColour = Color3.fromRGB(255, 255, 0);
+
+	};
+	["Regular"] = { 
+		RankId = 0;
+
+	};
+	
 }
 
 module.RankBinds = {
@@ -22,8 +55,8 @@ module.RankBinds = {
 		--[group_id]  = {min_rank, rank id}
 		
 		[0] = {
-			Min_Group_Rank = 5;
-			Rank = 0;
+			Min_Group_Rank = 0;
+			RankId = 0;
 		}
 	};
 	Gamepasses = {
@@ -36,13 +69,22 @@ module.RankBinds = {
 	}
 }
 
+
 module.ChatTags = true; -- whether or not if chat  tags should appear for each staff rank (any rank with an id > 0)
 module.OverheadTags = {
 	Enabled = true; -- whether or not if there should be a tag over the player's head (any rank with an id > 0)
 	Command = "/tag"; -- what command can be used to enable or disable the tag
 }
 
-module.DatastoreName = "SRX_DEFAULT" -- !! CHANGE THIS TO WHATEVER YOU NEED IT TO BE !!
+module.DatastoreName = "SRX_DEFAULT_DS" -- !! CHANGE THIS TO WHATEVER YOU NEED IT TO BE || DO NOT LEAVE THE DATASTORE NAME AS IT CURRENTLY IS !!
+
+module.SaveRanks = true; -- whether or not if ranks transfer from server to server (permanent rank changes)
+
+
+module.VIPServerSettings = { -- settings for VIP servers
+	VIPCommands = false; -- whether vip server owners get commands or not
+	ServerOwnerRankId = 0; -- rank id for vip server owners
+}
 
 module.WebhookSettings = {
 	["COMMANDS"] = {
@@ -50,7 +92,7 @@ module.WebhookSettings = {
 		WebhookLink = ""; -- webhook link
 	};
 	["DEV_CONSOLE"] = {
-		Enabled = true; -- whether to log stuff that goes through the dev console or not
+		Enabled = true; -- whether to log commands that goes through the developer console or not || note that it is currently impossible to detect who inputted what into roblox's developer console
 		WebhookLink = "";
 	}
 }
