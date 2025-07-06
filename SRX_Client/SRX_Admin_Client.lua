6f34b61151bc9e5f02bbd256c9dae6b648ecb93d
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-07-06 12:27:10 -0400

    Update SRX_Admin_Client.lua

--- SRX_Client/SRX_Admin_Client.lua
+++ SRX_Client/SRX_Admin_Client.lua
@@ -3,8 +3,14 @@ local SRX_Events = game.ReplicatedStorage:FindFirstChild("SRX_Events")
 ----------------------------------------------------------------
 local Utilities = script.Parent.Utilities
 ----------------------------------------------------------------
+local CS_Func = SRX_Events:WaitForChild("CSC_Func")
+local CS_Event = SRX_Events:WaitForChild("CSC_Event")
+----------------------------------------------------------------
 
 local TCS = game:GetService("TextChatService")
+
+----------------------------------------------------------------
+local chatTagsEnabled = CS_Func:InvokeServer("ChatTagStatus")
 
 ----------------------------------------------------------------
 
@@ -12,14 +18,16 @@ local TCS = game:GetService("TextChatService")
 TCS.OnIncomingMessage = function(msg:TextChatMessage)
 	local ts = msg.TextSource
 	if ts then
-		local plr = game.Players:GetPlayerByUserId(ts.UserId)
-		if plr then
-			local rn = plr:GetAttribute("SRX_RANKNAME")
-			local rc = plr:GetAttribute("SRX_RANKCOLOUR")
-			if rn and rc then
-				local n_msg_prp = Instance.new("TextChatMessageProperties")
-				n_msg_prp.PrefixText = '<font color="#'..rc:ToHex()..'">['..tostring(rn)..']</font> '..msg.PrefixText
-				return n_msg_prp
+		if chatTagsEnabled then
+			local plr = game.Players:GetPlayerByUserId(ts.UserId)
+			if plr then
+				local rn = plr:GetAttribute("SRX_RANKNAME")
+				local rc = plr:GetAttribute("SRX_RANKCOLOUR")
+				if rn and rc then
+					local n_msg_prp = Instance.new("TextChatMessageProperties")
+					n_msg_prp.PrefixText = '<font color="#'..rc:ToHex()..'">['..tostring(rn)..']</font> '..msg.PrefixText
+					return n_msg_prp
+				end
 			end
 		end
 	end
@@ -27,3 +35,4 @@ end
 
 
 ----------------------------------------------------------------
+
