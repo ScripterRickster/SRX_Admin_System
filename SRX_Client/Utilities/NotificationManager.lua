93d454f6165a3e9ace9f8e8952454cdfb0cef7ec
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-07-08 20:33:55 -0400

    Update NotificationManager.lua

--- SRX_Client/Utilities/NotificationManager.lua
+++ SRX_Client/Utilities/NotificationManager.lua
@@ -1,3 +1,14 @@
 local module = {}
 
+module.GetClientTime = function(utcTime)
+	local utc_curr = os.time()
+	local client_curr = math.floor(tick())
+	local diff = client_curr - utc_curr
+	return utcTime + diff
+end
+
+-----------------------------------------
+
+
+
 return module
