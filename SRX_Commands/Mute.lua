a4df04b7434b18cb1f6b13634848636900dc4fc7
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-08-28 22:48:53 -0400

    Update Mute.lua

--- SRX_Commands/Mute.lua
+++ SRX_Commands/Mute.lua
@@ -52,11 +52,13 @@ module.Aliases = { -- other names that tie it to this command
 	-- "alias_name1";
 }
 
+module.SendLog = true -- whether the command is logged or not
 
 
 module.Execute = function(parameters:table)
 	-- !! BY DEFAULT, ALL PARAMETER TABLES WILL INCLUDE THE PERSON WHO EXECUTED THE COMMAND | IT WILL BE STORED IN AS "EXECUTOR" !!
 	
+	local execSuccess = false
 	local meetsRequirements = serverUtil.CheckCommandRequirements(module.Parameters,parameters)
 	
 	if meetsRequirements then
@@ -69,7 +71,7 @@ module.Execute = function(parameters:table)
 			if target  then
 				local tRankId,tRankName = playerUtil.GetPlayerRankInfo(target)
 				
-				if tRankId < e_rID and not target:GetAttribute("SRX_MUTED") then
+				if tRankId <= e_rID and not target:GetAttribute("SRX_MUTED") then
 					target:SetAttribute("SRX_MUTED",true)
 					local muteReason = parameters["REASON"]
 					
@@ -78,14 +80,14 @@ module.Execute = function(parameters:table)
 					muteReason = serverUtil.FilterMessage(executor,muteReason)
 			
 					
-					task.defer(function() -- notifies the server to log this command being run
-						serverUtil.LogCommand(script,parameters)
-					end)
+					execSuccess = true
 				end
 				
 			end
 		end
 	end
+	
+	return execSuccess
 	
 end
 
