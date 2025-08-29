d58bae5dc894eaf3d8682db8f42328581870616b
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-08-28 22:48:05 -0400

    Update Kick.lua

--- SRX_Commands/Kick.lua
+++ SRX_Commands/Kick.lua
@@ -52,11 +52,13 @@ module.Aliases = { -- other names that tie it to this command
 	-- "alias_name1";
 }
 
+module.SendLog = true -- whether the command is logged or not
 
 
 module.Execute = function(parameters:table)
 	-- !! BY DEFAULT, ALL PARAMETER TABLES WILL INCLUDE THE PERSON WHO EXECUTED THE COMMAND | IT WILL BE STORED IN AS "EXECUTOR" !!
 	
+	local execSuccess = false
 	local meetsRequirements = serverUtil.CheckCommandRequirements(module.Parameters,parameters)
 	
 	if meetsRequirements then
@@ -90,14 +92,14 @@ module.Execute = function(parameters:table)
 						target:Kick(kickReason)
 					end)
 					
-					task.defer(function() -- notifies the server to log this command being run
-						serverUtil.LogCommand(script,parameters)
-					end)
+					execSuccess = true
 				end
 				
 			end
 		end
 	end
+	
+	return execSuccess
 	
 end
 
