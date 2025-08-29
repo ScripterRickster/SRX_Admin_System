589616095e68adf8461f920c814b05899064bb60
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-08-28 22:44:32 -0400

    Update Announce.lua

--- SRX_Commands/Announce.lua
+++ SRX_Commands/Announce.lua
@@ -47,12 +47,14 @@ module.Aliases = { -- other names that tie it to this command
 	-- "alias_name1";
 }
 
+module.SendLog = true -- whether the command is logged or not
 
 local CSC_Event = EVENTS:WaitForChild("CSC_Event")
 
 module.Execute = function(parameters:table)
 	-- !! BY DEFAULT, ALL PARAMETER TABLES WILL INCLUDE THE PERSON WHO EXECUTED THE COMMAND | IT WILL BE STORED IN AS "EXECUTOR" !!
 	
+	local execSuccess = false
 	local meetsRequirements = serverUtil.CheckCommandRequirements(module.Parameters,parameters)
 	
 	if meetsRequirements then
@@ -67,13 +69,15 @@ module.Execute = function(parameters:table)
 				CSC_Event:FireAllClients("announcement",executor.UserId,filteredMsg)
 				
 				
-				task.defer(function() -- notifies the server to log this command being run
-					serverUtil.LogCommand(script,parameters)
-				end)
+				execSuccess = true
+				
+				
 			end
 			
 		end
 	end
+	
+	return execSuccess
 	
 end
 
