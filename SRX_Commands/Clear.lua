822191a9e7513484325fe247448364a451226e29
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-08-28 22:45:32 -0400

    Update Clear.lua

--- SRX_Commands/Clear.lua
+++ SRX_Commands/Clear.lua
@@ -41,10 +41,13 @@ module.Aliases = { -- other names that tie it to this command
 	-- "alias_name1";
 }
 
+module.SendLog = true -- whether the command is logged or not
+
 
 module.Execute = function(parameters:table)
 	-- !! BY DEFAULT, ALL PARAMETER TABLES WILL INCLUDE THE PERSON WHO EXECUTED THE COMMAND | IT WILL BE STORED IN AS "EXECUTOR" !!
 	
+	local execSuccess = false
 	local meetsRequirements = serverUtil.CheckCommandRequirements(module.Parameters,parameters)
 	
 	if meetsRequirements then
@@ -57,12 +60,12 @@ module.Execute = function(parameters:table)
 				v:Destroy()
 			end
 
-			task.defer(function() -- notifies the server to log this command being run
-				serverUtil.LogCommand(script,parameters)
-			end)
+			execSuccess = true
 
 		end
 	end
+	
+	return execSuccess
 	
 end
 
