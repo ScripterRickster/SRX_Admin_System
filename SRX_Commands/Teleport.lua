9f54bd220aa9daa2846da002a8ceb6ab5ceb3c63
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-08-28 22:52:06 -0400

    Update Teleport.lua

--- SRX_Commands/Teleport.lua
+++ SRX_Commands/Teleport.lua
@@ -35,7 +35,7 @@ module.Parameters = {
 	]]
 	
 	["TARGET"] = {
-		Description = "Target of the command";
+		Description = "The player you wish to teleport to";
 		Required = true;
 		Class = "User";
 	}
@@ -46,10 +46,12 @@ module.Aliases = { -- other names that tie it to this command
 	-- "alias_name1";
 }
 
+module.SendLog = true -- whether the command is logged or not
 
 module.Execute = function(parameters:table)
 	-- !! BY DEFAULT, ALL PARAMETER TABLES WILL INCLUDE THE PERSON WHO EXECUTED THE COMMAND | IT WILL BE STORED IN AS "EXECUTOR" !!
 	
+	local execSuccess = false
 	local meetsRequirements = serverUtil.CheckCommandRequirements(module.Parameters,parameters)
 	
 	if meetsRequirements then
@@ -68,13 +70,13 @@ module.Execute = function(parameters:table)
 
 				exec_hrp.CFrame = char_hrp.CFrame
 
-				task.defer(function() -- notifies the server to log this command being run
-					serverUtil.LogCommand(script,parameters)
-				end)
+				execSuccess = true
 			end
 
 		end
 	end
+	
+	return execSuccess
 	
 end
 
