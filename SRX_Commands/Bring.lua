76d6c33bc192e7b5ced906453e440155174ab070
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-08-28 22:45:11 -0400

    Update Bring.lua

--- SRX_Commands/Bring.lua
+++ SRX_Commands/Bring.lua
@@ -46,10 +46,12 @@ module.Aliases = { -- other names that tie it to this command
 	-- "alias_name1";
 }
 
+module.SendLog = true -- whether the command is logged or not
 
 module.Execute = function(parameters:table)
 	-- !! BY DEFAULT, ALL PARAMETER TABLES WILL INCLUDE THE PERSON WHO EXECUTED THE COMMAND | IT WILL BE STORED IN AS "EXECUTOR" !!
 	
+	local execSuccess = false
 	local meetsRequirements = serverUtil.CheckCommandRequirements(module.Parameters,parameters)
 	
 	if meetsRequirements then
@@ -71,15 +73,15 @@ module.Execute = function(parameters:table)
 					
 					char_hrp.CFrame = exec_hrp.CFrame
 					
-					task.defer(function() -- notifies the server to log this command being run
-						serverUtil.LogCommand(script,parameters)
-					end)
+					execSuccess = true
 				end
 
 			end
 
 		end
 	end
+	
+	return execSuccess
 	
 end
 
