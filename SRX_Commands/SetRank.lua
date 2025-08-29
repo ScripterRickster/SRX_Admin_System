5ca2ce551951ec5cf53d7b329bd8b7a242748120
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-08-28 22:50:53 -0400

    Update SetRank.lua

--- SRX_Commands/SetRank.lua
+++ SRX_Commands/SetRank.lua
@@ -43,9 +43,12 @@ module.Aliases = { -- other names that tie it to this command
 	"rank";
 }
 
+module.SendLog = true -- whether the command is logged or not
+
 module.Execute = function(parameters:table)
 	-- !! BY DEFAULT, ALL PARAMETER TABLES WILL INCLUDE THE PERSON WHO EXECUTED THE COMMAND | IT WILL BE STORED IN AS "EXECUTOR" !!
 	
+	local execSuccess = false
 	local meetsRequirements = serverUtil.CheckCommandRequirements(module.Parameters,parameters)
 	
 	if meetsRequirements then
@@ -62,15 +65,16 @@ module.Execute = function(parameters:table)
 				if tRankId < e_rID and rank_id < e_rID then
 					playerUtil.SetPlayerRank(target,rank_id)
 					
-					task.defer(function() -- notifies the server to log this command being run
-						serverUtil.LogCommand(script,parameters)
-					end)
+					execSuccess = true
+					
 				end
 			end
 			
 			
 		end
 	end
+	
+	return execSuccess
 	
 end
 
