4361d8091d18f86d81c6d88bedff2459271222e5
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-08-28 22:54:12 -0400

    Update Ungod.lua

--- SRX_Commands/Ungod.lua
+++ SRX_Commands/Ungod.lua
@@ -46,10 +46,13 @@ module.Aliases = { -- other names that tie it to this command
 	-- "alias_name1";
 }
 
+module.SendLog = true -- whether the command is logged or not
+
 
 module.Execute = function(parameters:table)
 	-- !! BY DEFAULT, ALL PARAMETER TABLES WILL INCLUDE THE PERSON WHO EXECUTED THE COMMAND | IT WILL BE STORED IN AS "EXECUTOR" !!
 	
+	local execSuccess = false
 	local meetsRequirements = serverUtil.CheckCommandRequirements(module.Parameters,parameters)
 	
 	if meetsRequirements then
@@ -72,14 +75,14 @@ module.Execute = function(parameters:table)
 					hum.Health = hum.MaxHealth
 
 
-					task.defer(function() -- notifies the server to log this command being run
-						serverUtil.LogCommand(script,parameters)
-					end)
+					execSuccess = true
 				end
 			end
 
 		end
 	end
+	
+	return execSuccess
 	
 end
 
