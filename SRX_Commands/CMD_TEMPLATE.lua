1a551db79b1d95a1384c5a8c2e6c6263401b857c
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-08-28 22:39:32 -0400

    Update CMD_TEMPLATE.lua

--- SRX_Commands/CMD_TEMPLATE.lua
+++ SRX_Commands/CMD_TEMPLATE.lua
@@ -42,10 +42,12 @@ module.Aliases = { -- other names that tie it to this command
 	-- "alias_name1";
 }
 
+module.SendLog = true -- whether the command is logged or not
 
 module.Execute = function(parameters:table)
 	-- !! BY DEFAULT, ALL PARAMETER TABLES WILL INCLUDE THE PERSON WHO EXECUTED THE COMMAND | IT WILL BE STORED IN AS "EXECUTOR" !!
 	
+	local execSuccess = false
 	local meetsRequirements = serverUtil.CheckCommandRequirements(module.Parameters,parameters)
 	
 	if meetsRequirements then
@@ -54,14 +56,14 @@ module.Execute = function(parameters:table)
 		
 		if serverUtil.PlayerCanUseCommand(executor,script) then
 			
-			task.defer(function() -- notifies the server to log this command being run
-				serverUtil.LogCommand(script,parameters)
-			end)
+			execSuccess = true
 			
 			
 			-- do whatever here
 		end
 	end
+	
+	return execSuccess
 	
 end
 
