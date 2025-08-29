31fcb4638aefc0826ba97349f1f735fa16ae8717
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-08-28 22:54:30 -0400

    Update Unmute.lua

--- SRX_Commands/Unmute.lua
+++ SRX_Commands/Unmute.lua
@@ -38,6 +38,7 @@ module.Parameters = {
 		Description = "Target of the command";
 		Required = true; 
 		Class = "User";
+
 	};
 	
 }
@@ -47,11 +48,14 @@ module.Aliases = { -- other names that tie it to this command
 	-- "alias_name1";
 }
 
+module.SendLog = true -- whether the command is logged or not
+
 
 
 module.Execute = function(parameters:table)
 	-- !! BY DEFAULT, ALL PARAMETER TABLES WILL INCLUDE THE PERSON WHO EXECUTED THE COMMAND | IT WILL BE STORED IN AS "EXECUTOR" !!
 	
+	local execSuccess = false
 	local meetsRequirements = serverUtil.CheckCommandRequirements(module.Parameters,parameters)
 	
 	if meetsRequirements then
@@ -68,15 +72,14 @@ module.Execute = function(parameters:table)
 			
 					target:SetAttribute("SRX_MUTED",false)
 					
-					task.defer(function() -- notifies the server to log this command being run
-						serverUtil.LogCommand(script,parameters)
-					end)
+					execSuccess = true
 				end
 				
 			end
 		end
 	end
 	
+	return execSuccess
 end
 
 return module
