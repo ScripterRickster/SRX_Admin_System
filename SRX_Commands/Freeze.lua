823c49afa27a0654bce998f07b41265a1b2f20e4
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-08-28 22:46:08 -0400

    Update Freeze.lua

--- SRX_Commands/Freeze.lua
+++ SRX_Commands/Freeze.lua
@@ -47,11 +47,14 @@ module.Aliases = { -- other names that tie it to this command
 	"anchor";
 }
 
+module.SendLog = true -- whether the command is logged or not
+
 local frozenTag = ASSETS:WaitForChild("SRX_FROZENTAG")
 
 module.Execute = function(parameters:table)
 	-- !! BY DEFAULT, ALL PARAMETER TABLES WILL INCLUDE THE PERSON WHO EXECUTED THE COMMAND | IT WILL BE STORED IN AS "EXECUTOR" !!
 	
+	local execSuccess = false
 	local meetsRequirements = serverUtil.CheckCommandRequirements(module.Parameters,parameters)
 	
 	if meetsRequirements then
@@ -77,15 +80,15 @@ module.Execute = function(parameters:table)
 						frozenTag:Clone().Parent = char:WaitForChild("Head")
 						
 						
-						task.defer(function() -- notifies the server to log this command being run
-							serverUtil.LogCommand(script,parameters)
-						end)
+						execSuccess = true
 					end
 				end
 			end
 
 		end
 	end
+	
+	return execSuccess
 	
 end
 
