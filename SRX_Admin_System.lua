1ebe14259c866baf6ca985fdea0b82f9921fe40e
Ricky L. <101468308+ScripterRickster@users.noreply.github.com>
2025-11-21 22:39:34 -0500

    Add system access type retrieval in SRX_Admin_System

--- SRX_Admin_System.lua
+++ SRX_Admin_System.lua
@@ -243,6 +243,8 @@ CSC_Func.OnServerInvoke = function(plr:Player,param1,param2,param3,param4,param5
 			return "rbxassetid://0"
 		elseif param1 == "getplayerinfo" and param2 then
 			return plrUtilities.GetPlayerInformation(param2)
+		elseif param1 == "getsysaccesstype" then
+			return adminSettings.SystemAccessType
 		end
 	end
 end
